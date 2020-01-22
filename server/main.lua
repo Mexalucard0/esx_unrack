86691d3dbaaa093891fef953f2a6bb2e4a1f8c99
xavlios <5066808+xavlios@users.noreply.github.com>
2020-01-22 08:12:24 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,10 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+-- on player load check if they are in dataase
+-- if not add them and 2 lines 1 for sniper and 1 for rifle
+-- set both to not stored and no attachments
+
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
 	ESX.PlayerData = xPlayer
